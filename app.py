07927ba4727c071552a84ae14d3bbcc752ca220c
Akshat Jain <78645765+akshat-jn-crypto@users.noreply.github.com>
2025-04-06 15:22:28 +0530

    data.pkl

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+df = pickle.load(open('data.pkl','rb'))
 
 st.title("Laptop Predictor")
 
